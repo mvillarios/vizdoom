4c891e34cb10e702b4328be8bd856a0c51842366
mvillarios <mvillarios@mvilla2019@inf.udec.cl>
2024-11-23 14:45:05 -0300

    Intento 2 last corridor. Mas varianza

--- sb-code/main.py
+++ sb-code/main.py
@@ -48,13 +48,13 @@ TRAINING_TIMESTEPS = int(1e6)  # 600k 200k 1000k
 N_ENVS = 1
 FRAME_SKIP = 4
 
-old_save = False
+old_save = True
 old_dir_dqn = "trains/corridor/dqn-1"
-old_dir_ppo = "trains/corridor/ppo-4-last"
+old_dir_ppo = "trains/corridor/ppo-1-last"
 
 #num = f"2-btn(menos)-fs({FRAME_SKIP})-steps({TRAINING_TIMESTEPS})"
 #num = f"4-fs({FRAME_SKIP})-steps({TRAINING_TIMESTEPS})"
-num = f"extra-last"
+num = f"ppo-2-last-2"
 
 class RewardShapingWrapper(RewardWrapper):
     def __init__(self, env, damage_reward=100, hit_taken_penalty=-5, ammo_penalty=-1):
